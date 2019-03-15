b86b1015d7b90055b1e472210255823e76b6fc76
Kirill <kirill.hawk@gmail.com>
2019-03-15 15:28:55 +0300

    trash
    trash

--- ekoshop_test_search/test_search.py
+++ ekoshop_test_search/test_search.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-capabilities = {
+capbilities = {
     "browserName": "chrome",
     "version": "72.0",
     "enableVNC": True,
@@ -11,13 +11,16 @@ capabilities = {
 }
 
 driver = webdriver.Remote(
- command_executor="http://localhost:4444/wd/hub",
-  desired_capabilities=capabilities)
-driver.get("http://shop.ekodar.ru")
+    command_executor="http://localhost:4444/wd/hub",
+    desired_capabilities=capbilities)
+driver.maximize_window()
 
+#def test_search_shop(self):
+driver.get("http://shop.ekodar.ru")
 elem = driver.find_element_by_css_selector(".btn.btn-green").click()
 time.sleep(2)
-elem = driver.find_element_by_xpath("//span/i").click()
+#Для браузера не в полный экран
+#elem = driver.find_element_by_xpath("//span/i").click()
 elem = driver.find_element_by_xpath("//input").send_keys("test")
 elem = driver.find_element_by_xpath("//input").send_keys(Keys.RETURN)
 time.sleep(5)
