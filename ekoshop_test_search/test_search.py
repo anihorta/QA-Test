d79b66f5ef4f200e0b3926bab159e54e2702bfc8
ivonderkof <47137545+ivonderkof@users.noreply.github.com>
2019-03-17 20:44:28 +0300

    Update test_search.py

--- ekoshop_test_search/test_search.py
+++ ekoshop_test_search/test_search.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-capbilities = {
+capabilities = {
     "browserName": "chrome",
     "version": "72.0",
     "enableVNC": True,
@@ -12,7 +12,7 @@ capbilities = {
 
 driver = webdriver.Remote(
     command_executor="http://localhost:4444/wd/hub",
-    desired_capabilities=capbilities)
+    desired_capabilities=capabilities)
 driver.maximize_window()
 
 driver.get("http://shop.ekodar.ru")
